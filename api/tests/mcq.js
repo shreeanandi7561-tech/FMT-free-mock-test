29ba606d3add4d08008383b2641d5fcba54c5e64
shreeanandi7561-tech <shreeanandi.7561@gmail.com>
2025-09-29 12:48:19 +0530

    Update mcq.js

--- api/tests/mcq.js
+++ api/tests/mcq.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // CORS Headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
